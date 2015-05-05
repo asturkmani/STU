aab38d43dc7cd85a692943497c859f3d78898ec9
asturkmani <asturkmani@gmail.com>
2015-05-05 21:31:37 +0300

    finalized project with report, datasheet and user manual

--- Code/InfraredNoInterrupt/InfraredNoInterrupt.ino
+++ Code/InfraredNoInterrupt/InfraredNoInterrupt.ino
@@ -1,31 +1,34 @@
-int sensorPin = A0;
-int refPin= A1;
-int refValue=0;
-Â
+int sensorPin = A1;
 int sensorValue = 0;
 int sumValue = 0;
 int avgValue = 0;
 int count = 0;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly: 
   sensorValue = analogRead(sensorPin);
-  refValue=analogRead(refPin);
-//  Serial.println(sensorValue);
-  sumValue = sensorValue + sumValue-refValue;
+  Serial.println(sensorValue);
+  delay(1);
+//  sensorValue2 = analogRead(sensor2);
   
-  if(count==200){
-    avgValue = sumValue/count;
-    Serial.println(avgValue);
-    sumValue = 0;
-    avgValue = 0;
-    count =0;
-     delay(1000);
-  }
-  count = count + 1;
+//  sumValue = sensorValue + sumValue;
+//  sumValue2 = sensorValue2 + sumValue2;
+  
+//  if(count==300){
+//    avgValue = sumValue/count;
+////    avgValue2 = sumValue2/count;
+//    Serial.println(avgValue);
+//    sumValue = 0;
+//    avgValue = 0;
+//    sumValue2 = 0;
+//    avgValue2 = 0;
+//    count =0;
+//    delay(1000);
+//  }
+//  count = count + 1;
 }
